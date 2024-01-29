a730b553cfdd7684859fc841877c9ad0e734f9e2
Sina Nazem <92341812+sinanazem@users.noreply.github.com>
2024-01-29 17:56:51 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/blog")
+def blog():
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
